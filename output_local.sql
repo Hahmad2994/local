5d09ffd85110681102cb7d82af7c55cc01b25038
Hahmad2994 <91420262+Hahmad2994@users.noreply.github.com>
2023-11-29 11:53:32 +0500

    hussain

--- output_local.sql
+++ output_local.sql
@@ -1,7 +1,7 @@
 
 UPDATE SUMMARY
-Run:                          2
-Previously run:              11
+Run:                          3
+Previously run:              10
 Filtered out:                 0
 -------------------------------
 Total change sets:           13
